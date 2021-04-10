328b6f4c1bff2ae66f1fe16b6758bcdf966b4a04
Martin Tomasi <martin.tomasi@gmail.com>
2021-04-10 22:51:33 +0200

    add some styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
-import { AppProps } from 'next/dist/next-server/lib/router/router'
-import { GlobalStyles } from 'twin.macro'
+import { AppProps } from "next/dist/next-server/lib/router/router";
+import { GlobalStyles } from "twin.macro";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 const App = ({ Component, pageProps }: AppProps) => (
   <div>
     <GlobalStyles />
     <Component {...pageProps} />
   </div>
-)
+);
 
-export default App
+export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,37 @@
+import {
+  fa500px,
+  faGithub,
+  faLastfm,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Head from "next/head";
 import tw from "twin.macro";
+
+const linkData = [
+  {
+    icon: faGithub,
+    text: "GitHub",
+    url: "https://github.com/GyrosOfWar",
+  },
+  {
+    icon: fa500px,
+    text: "500px",
+    url: "https://500px.com/p/martintomasi",
+  },
+  {
+    icon: faTwitter,
+    text: "Twitter",
+    url: "https://twitter.com/GyrosOfWar_",
+  },
+  {
+    icon: faLastfm,
+    text: "Last.fm",
+    url: "https://www.last.fm/user/GyrosOfWar",
+  },
+];
+
+const ExternalLink = tw.a`text-blue-400 w-full text-center hover:(bg-blue-400 text-white) transition p-2 border border-blue-400 flex justify-between items-center`;
 
 export default function Home() {
   return (
@@ -9,37 +41,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main css={tw`container ml-auto mr-auto p-1`}>
+      <main css={tw`container ml-auto mr-auto px-2 flex flex-col items-center`}>
         <div css={tw`my-4`}>
           <h1 css={tw`text-4xl font-bold`}>Martin Tomasi</h1>
         </div>
-        <p css={tw`mb-4`}>Hi, I'm a software developer from Vienna.</p>
+        <p css={tw`mb-4 text-lg`}>Hi, I'm a software developer from Vienna.</p>
 
-        <ul css={tw``}>
-          <li>
-            <a
-              css={tw`text-blue-400 hover:text-blue-500`}
-              href="https://github.com/GyrosOfWar"
-            >
-              GitHub
-            </a>
-          </li>
-          <li>
-            <a
-              css={tw`text-blue-400 hover:text-blue-500`}
-              href="https://twitter.com/GyrosOfWar_"
-            >
-              Twitter
-            </a>
-          </li>
-          <li>
-            <a
-              css={tw`text-blue-400 hover:text-blue-500`}
-              href="https://500px.com/p/martintomasi"
-            >
-              500px
-            </a>
-          </li>
+        <ul css={tw`flex flex-col text-lg gap-2 w-full md:w-64 text-center`}>
+          {linkData.map(({ icon, text, url }) => (
+            <li key={text}>
+              <ExternalLink href={url}>
+                <FontAwesomeIcon icon={icon} size="lg" />
+                <span>{text}</span>
+              </ExternalLink>
+            </li>
+          ))}
         </ul>
       </main>
     </>
