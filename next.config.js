85a2dc57ef1114540fc84e4fb90cd28d0b74a326
Martin Tomasi <martin.tomasi@gmail.com>
2021-06-21 18:33:07 +0200

    Update nextjs version

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  webpack: (config, {isServer}) => {
-    if (!isServer) {
-      // Unset client-side javascript that only works server-side
-      // https://github.com/vercel/next.js/issues/7755#issuecomment-508633125
-      config.node = {fs: "empty", module: "empty"}
-    }
+  webpack: (config) => {
+    // Unset client-side javascript that only works server-side
+    config.resolve.fallback = {fs: false, module: false}
 
     return config
   },
